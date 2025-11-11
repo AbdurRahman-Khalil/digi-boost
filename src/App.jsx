6522db05128367d1672e4e15f979fd538bd24713
AbdurRahman-Khalil <m.abdurrahmankhalil2@gmail.com>
2025-11-11 19:47:23 +0500

    fix deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import About from './components/About/About';
-import Services from './components/Services/Services';
-import Pricing from './components/Pricing/Pricing';
-import Footer from './components/Footer/Footer';
+import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
+import About from './components/about/About';
+import Services from './components/services/Services';
+import Pricing from './components/pricing/Pricing';
+import Footer from './components/footer/Footer';
 import styles from './App.module.css';
 
 
